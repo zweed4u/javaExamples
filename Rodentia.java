d0d54ce70157b5a392294e2ccf3092fc5401930c
root <zdw7287@rit.edu>
2016-03-16 23:43:17 -0400

    bulk commit

---
+++ Rodentia.java
@@ -0,0 +1,13 @@
+public class Rodentia {
+
+	public static void main(String[] args) {
+
+	}
+
+	public void order() {
+
+		System.out.println("This animal belongs to the Rodentia order.");
+
+	}
+
+}
